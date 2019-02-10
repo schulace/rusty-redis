7a29cd16738936b12665d84f8eaadb196f1ae4c0
schulace <schulace2@gmail.com>
2019-02-10 13:18:59 -0500

    now reads 1 command from the stream, handles it, sends back some data, and closes the connection

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,30 @@
 use std::collections::HashMap;
-use std::error::Error;
-use std::io;
+use std::io::prelude::*;
+use std::net::{Shutdown, TcpListener};
 
 mod parser;
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() {
   let mut map: HashMap<String, String> = HashMap::new();
-  loop {
-    let mut s = String::new();
-    io::stdin().read_line(&mut s)?;
-    println!("{}", handle_query(s, &mut map));
+  let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+  for stream in listener.incoming() {
+    let mut buffer = [0; 512];
+    let mut stream = stream.unwrap();
+    stream.read(&mut buffer).unwrap();
+    eprintln!(
+      "read \"{}\" from the stream",
+      std::str::from_utf8(&buffer).unwrap()
+    );
+    let s = handle_query(std::str::from_utf8(&buffer).unwrap(), &mut map);
+    stream.write_all(&s.as_bytes()).unwrap();
+    stream.shutdown(Shutdown::Both).unwrap();
+    eprintln!("wrote {} to the socket", &s);
   }
 }
 
-fn handle_query(query_str: String, map: &mut HashMap<String, String>) -> String {
+fn handle_query(query_str: &str, map: &mut HashMap<String, String>) -> String {
   use parser::Command::*;
-  if let Ok(query) = parser::parse_query(&query_str) {
+  if let Ok(query) = parser::parse_query(query_str) {
     match query {
       GET(k) => map.get(&k).unwrap_or(&"Not in map".to_string()).to_owned(),
       SET(k, v) => map.insert(k, v).map_or_else(
@@ -24,12 +33,16 @@ fn handle_query(query_str: String, map: &mut HashMap<String, String>) -> String 
       ),
       INCR(k) => {
         let ent = map.entry(k).and_modify(|st: &mut String| {
-        if let Ok(i) = st.parse::<i128>() {
-          *st = (i + 1).to_string();
-        }});
+          if let Ok(i) = st.parse::<i128>() {
+            *st = (i + 1).to_string();
+          }
+        });
         let k = ent.key().clone();
-        map.get(&k).unwrap_or(&"key not in map".to_string()).to_owned()
-      },
+        map
+          .get(&k)
+          .unwrap_or(&"key not in map".to_string())
+          .to_owned()
+      }
       DECR(k) => {
         let ent = map.entry(k).and_modify(|st: &mut String| {
           if let Ok(i) = st.parse::<i128>() {
@@ -37,7 +50,10 @@ fn handle_query(query_str: String, map: &mut HashMap<String, String>) -> String 
           }
         });
         let k = ent.key().clone();
-        map.get(&k).unwrap_or(&"key not in map".to_string()).to_owned()
+        map
+          .get(&k)
+          .unwrap_or(&"key not in map".to_string())
+          .to_owned()
       }
     }
   } else {
